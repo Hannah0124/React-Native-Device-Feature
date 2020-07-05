ae0b2d20a1105d8b0310be5ff7ff1dd9b1e1a2c7
Hannah <hannahjoo24@gmail.com>
2020-07-04 23:27:20 -0700

    Storing the image on the FileSystem

--- store/places-actions.js
+++ store/places-actions.js
@@ -1,5 +1,24 @@
+import * FileSystem from 'expo-file-system';
+
 export const ADD_PLACE = 'ADD_PLACE';
 
 export const addPlace = (title, image) => {
-  return { type: ADD_PLACE, placeData: { title: title, image: image } };
+  return async dispatch => {
+    
+    const fileName = image.split('/').pop();
+    const newPath = FileSystem.documentDirectory + fileName;
+
+    // I want to move a file here
+    try {
+      await FileSystem.moveAsync({
+        from: image, // where the file currently sits
+        to: newPath
+      });
+    } catch(err) {
+      console.log(err);
+      throw err;
+    }
+
+    dispatch({ type: ADD_PLACE, placeData: { title: title, image: newPath } });
+  };
 };
